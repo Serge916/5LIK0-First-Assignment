3e2c603424c0296b63e508cc4fdc76553cc4f748
SergioGeronimoParras <sergio.g.p@hotmail.es>
2024-05-24 20:35:09 +0200

    Problems 2 and 3 (except e)

--- assignment1_2.m
+++ assignment1_2.m
@@ -4,13 +4,20 @@ clear;
 load("data2.mat")
 
 % part a
-
+%Check for sigma>5, take that unitary vector and shorten it a bit
+[U,S,V]=svd(A);
+v = .99*V(:,1);
 
 % part b
-
-
+%Sigma3=1, x1 is therefore v(3)
+%x2 should be searchable by adding v(3) to v(2)
+x1 = V(:,3);
+x2 = V(:,2)-1.291*V(:,3);
+B=[x1;x2];
 % part c
-
+% CN=sigma1/sigmar; norm(C)=3=sigma1; rank(2)=>sigmar=sigma2
+% U and V can be Identity. Then, C = USV=S
+C = [diag([3 .75]);[0 0]];
 
 % clear eveything except the required answers
 clearvars -EXCEPT v B C;
--- assignment1_3.m
+++ assignment1_3.m
@@ -1,16 +1,28 @@
 clear;
 
 % part a
-
+% Vectors in unitary matrices form an orthonormal basis. u(1)*u(2)=0
+V = [.9602 -.2794; .2794 .9602];
 
 % part b
-
+% Any orthonormal basis constitutes an unitary matrix. Matrix randomly
+% obtained (randn(3,3)).
+U = orth([.5377 .8622 -.4336; 1.8339 .3188 .3426; -2.2588 -1.3077 3.5784]);
 
 % part c
-
+S = [diag([3 2]); [0 0]];
+A = U*S*V';
 
 % part d
-
+% When in the basis, norm(Ax)=sigma(i)*norm(x)
+% We are looking for a vector that doesn't fully align with the major basis
+% vector. We can consider this vector as norm(x)=1
+% norm(Ax)=norm(Sy)=sqrt((s(1)*y(1))^2+(s(2)*y(2))^2)=sqrt(13/2)
+y1 = roots([2*3^2-2*2^2 0 2*2^2-13]);
+y1 = y1(1); %Take just one solution
+y2 = sqrt(1-y1^2); %Norm(y)=1
+y = [y1;y2];
+x = V*y; % y=V'*x
 
 % part e
 
