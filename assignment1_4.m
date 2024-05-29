57ee2584590dd95d62cc57fdaf700f2a89a4ac24
SergioGeronimoParras <sergio.g.p@hotmail.es>
2024-05-29 14:40:19 +0200

    Problem 4

--- assignment1_4.m
+++ assignment1_4.m
@@ -3,6 +3,15 @@ clear;
 % Load the variables ....
 load("data4.mat")
 
+X = [cos(deg2rad(x));
+    sin(deg2rad(y));
+    ones(1,length(x))];
+W = z*pinv(X);
+a=W(1);
+b=W(2);
+c=W(3);
+
+E = sum(abs(z-W*X));
 
 
 % clear eveything except the required answers
